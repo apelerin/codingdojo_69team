cf78622b872eb81a39d164edc4919d104b619778
HugoMonnerie <hugomonnerie32@gmail.com>
2019-09-17 15:18:41 +0200

    Mon avancement

--- src/main/java/fr/rphstudio/codingdojo/students/Student69.java
+++ src/main/java/fr/rphstudio/codingdojo/students/Student69.java
@@ -13,32 +13,52 @@ import fr.rphstudio.codingdojo.game.PodPlugIn;
  * @author Romuald GRIGNON
  */
 public class Student69 extends PodPlugIn {
-    public Student69(Pod p){
+    public Student69(Pod p) {
         super(p);
     }
-    
+
     //-------------------------------------------------------
     // DECLARE YOUR OWN VARIABLES AND FUNCTIONS HERE
+    float indcheck;
+    float Xship;
+    float Yship;
+    float Xcheck;
+    float Ycheck;
+    float angleship;
+    float Xdist;
+    float Ydist;
 
-    
-    
+
     // END OF VARIABLES/FUNCTIONS AREA
     //-------------------------------------------------------
-    
+
     @Override
-    public void process(int delta)
-    {   
+    public void process(int delta) {
         //-------------------------------------------------------
         // WRITE YOUR OWN CODE HERE
-        
-        setPlayerName("69Student");
-        selectShip(1);
-        setPlayerColor(255,255,255,255);
+        indcheck = getNextCheckPointIndex();
+        Xship = getShipPositionX();
+        Yship = getShipPositionY();
+        Xcheck = getNextCheckPointX();
+        Ycheck = getNextCheckPointY();
+        angleship = getShipAngle();
+        Xdist = Xcheck - Xship;
+        Ydist = Ycheck - Yship;
 
-        moveToNextCheckPoint(0.5f);
-
+        setPlayerName("BANANA 69");
+        selectShip(32);
+        setPlayerColor(255, 0, 255, 130);
+        if (getShipAngle()==atan2(Ydist,Xdist)*180/3.14){
+            incSpeed(1.0f);
+        }
+        else{
+            turn(5f);
+        }
         // END OF CODE AREA
         //-------------------------------------------------------
-    }
-    
+        }
+
 }
+
+    
+
