4856c3a4207ebd23f6261e80afe4001bb0d87ef8
AlyssiaPH <Alyssia0207@gmail.com>
2019-09-18 14:56:02 +0200

    fini 5 track

--- src/main/java/fr/rphstudio/codingdojo/students/Student69.java
+++ src/main/java/fr/rphstudio/codingdojo/students/Student69.java
@@ -64,27 +64,29 @@ public class Student69 extends PodPlugIn {
 
         float dist= calcdist(procheck);
 
-        if (dist <=2.2){
-            ciblecheck= procheck+1;
-            calcdist(ciblecheck);
-         }
+        if (dist <=2){
+            if (procheck==getNbRaceCheckPoints()) {
+                ciblecheck = 0; }
 
+            else {
+                ciblecheck= procheck+1;}
+
+            calcdist(ciblecheck);
+
+        }
         float diffangle = calcangle(ciblecheck);
         turn(diffangle);
 
-        if (getShipSpeed()<=1f) {
-            incSpeed(0.45f);
+        if ((getShipSpeed()<=2f) & (diffangle==0)) {
+            incSpeed(0.85f);
         }
         else{
-            if (dist <= 3) {
-                incSpeed(-0.9f*getShipSpeed());
+            if (dist <= 3.5) {
+                incSpeed(-0.85f*getShipSpeed());
             }
              else if (dist >=8 & (getShipBoostLevel()==100) & (diffangle==0)) {
                      useBoost();
                  }
-             else if (dist >=6.5) {
-                incSpeed(1);
-            }
              else{
                     incSpeed(1f);
                 }}
