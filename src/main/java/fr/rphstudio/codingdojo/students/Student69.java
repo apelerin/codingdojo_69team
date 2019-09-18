1c617d015ba4ed9f14bdefde41a7161a6e1b1133
AlyssiaPH <Alyssia0207@gmail.com>
2019-09-18 15:53:11 +0200

    update

--- src/main/java/fr/rphstudio/codingdojo/students/Student69.java
+++ src/main/java/fr/rphstudio/codingdojo/students/Student69.java
@@ -57,34 +57,30 @@ public class Student69 extends PodPlugIn {
 
         int procheck = getNextCheckPointIndex();
         int ciblecheck = procheck;
-        float xcheck = getCheckPointPositionX(procheck);
-        float ycheck = getCheckPointPositionY(procheck);
-        float xship = getShipPositionX();
-        float yship = getShipPositionY();
 
-        float dist= calcdist(procheck);
+        float dist = calcdist(procheck);
 
         if (dist <=2){
-            if (procheck==getNbRaceCheckPoints()) {
+            if (procheck == getNbRaceCheckPoints()) {
                 ciblecheck = 0; }
 
             else {
-                ciblecheck= procheck+1;}
+                ciblecheck = procheck+1;}
 
             calcdist(ciblecheck);
-
         }
+
         float diffangle = calcangle(ciblecheck);
         turn(diffangle);
 
-        if ((getShipSpeed()<=2f) & (diffangle==0)) {
+        if ((getShipSpeed() <= 2f) & (diffangle ==0 )) {
             incSpeed(0.85f);
         }
         else{
             if (dist <= 3.5) {
-                incSpeed(-0.85f*getShipSpeed());
+                incSpeed(-0.85f * getShipSpeed());
             }
-             else if (dist >=8 & (getShipBoostLevel()==100) & (diffangle==0)) {
+             else if (dist >=8 & (getShipBoostLevel() == 100) & (diffangle == 0)) {
                      useBoost();
                  }
              else{
